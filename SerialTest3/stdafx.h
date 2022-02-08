a906fad652c58ecab555c3ee318ec13205e259de
chrishoen <chris123hoen@gmail.com>
2022-02-08 09:42:11 -0500

    more

--- SerialTest3/stdafx.h
+++ SerialTest3/stdafx.h
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <math.h>
 #include <random>
 
